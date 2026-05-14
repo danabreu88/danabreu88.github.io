13cd5fb100022c529b7a80fe8dd5c293445765de
danabreu88 <dan.abreu88@gmail.com>
2026-05-14 11:30:24 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add any JavaScript functionality here
+// For example, you could add smooth scrolling to page anchors
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
